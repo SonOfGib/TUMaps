5c119e779f166ea839e6819a5fdf2ea6076400ce
Will Johannesen <willjohannesen@gmail.com>
2019-03-26 11:06:50 -0400

    Wiki displays

--- app/src/main/java/edu/temple/basic/MainActivity.java
+++ app/src/main/java/edu/temple/basic/MainActivity.java
@@ -2,6 +2,7 @@ package edu.temple.basic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView myWebView = findViewById(R.id.webView);
+        myWebView.loadUrl("http://ec2-35-170-187-151.compute-1.amazonaws.com/");
     }
 }
