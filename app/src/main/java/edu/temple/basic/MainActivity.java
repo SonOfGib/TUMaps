b1f53c1d497c1ca7d8b37c603cb12f6e1f2b17b6
Will Johannesen <willjohannesen@gmail.com>
2019-04-09 11:03:02 -0400

    map displays

--- app/src/main/java/edu/temple/basic/MainActivity.java
+++ app/src/main/java/edu/temple/basic/MainActivity.java
@@ -1,17 +1,116 @@
 package edu.temple.basic;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    LocationManager lm;
+    LocationListener ll;
+    private GoogleMap mMap;
+    Marker lastMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView myWebView = findViewById(R.id.webView);
-        myWebView.loadUrl("http://ec2-34-203-104-209.compute-1.amazonaws.com/");
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        SupportMapFragment mapFragment = new SupportMapFragment();
+        transaction.replace(R.id.mapView, mapFragment).commit();
+        mapFragment.getMapAsync(MainActivity.this);
+
+        lm = getSystemService(LocationManager.class);
+        ll = makeLocationListener();
+
+        //WebView myWebView = findViewById(R.id.webView);
+        //myWebView.loadUrl("http://ec2-34-203-104-209.compute-1.amazonaws.com/");
+    }
+
+    private LocationListener makeLocationListener(){
+        return new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                if (lastMarker != null)
+                    lastMarker.setPosition(latLng);
+                 else {
+                    MarkerOptions markerOptions = (new MarkerOptions())
+                            .position(latLng)
+                            .title("You");
+
+                    lastMarker = mMap.addMarker(markerOptions);
+                }
+
+                CameraUpdate cameraUpdate = CameraUpdateFactory
+                        .newLatLngZoom(lastMarker.getPosition(), 16);
+
+                mMap.moveCamera(cameraUpdate);
+            }
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+            @Override
+            public void onProviderEnabled(String provider) { }
+            @Override
+            public void onProviderDisabled(String provider) { }
+        };
+    }
+
+    /**
+     * Map stuff
+     */
+    @Override
+    public void onMapReady(GoogleMap map) {mMap = map; }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            registerForLocationUpdates();
+        else
+            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 111); // make a constant reference
+    }
+
+    @SuppressLint("MissingPermission")
+    private void registerForLocationUpdates() {
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ll);
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, ll);
+        lm.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, ll);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            registerForLocationUpdates();
+        else
+            Toast.makeText(this, "No map permission", Toast.LENGTH_LONG).show();
     }
 }
