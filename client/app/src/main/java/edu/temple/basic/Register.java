afa8e94324a52d49a6c3b63d0d8548a0137eabe8
SonOfGib <seanmoner@gmail.com>
2019-04-29 23:49:02 -0400

    Users can now register in app!

--- client/app/src/main/java/edu/temple/basic/Register.java
+++ client/app/src/main/java/edu/temple/basic/Register.java
@@ -1,13 +1,16 @@
 package edu.temple.basic;
 
 import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -17,6 +20,8 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.util.HashMap;
@@ -64,16 +69,40 @@ public class Register extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mUsername = userName.getText().toString();
+                if (TextUtils.isEmpty(mUsername)) userName.setError("required");
                 mPassword = password.getText().toString();
+                if (TextUtils.isEmpty(mPassword)) password.setError("required");
+                mEmail = email.getText().toString();
+                if (TextUtils.isEmpty(mEmail)) email.setError("required");
+                mPasswordConf = passwordConf.getText().toString();
+                if (TextUtils.isEmpty(mPasswordConf)) passwordConf.setError("required");
+                mRealName = realName.getText().toString();
+                if (TextUtils.isEmpty(mUsername)) mRealName ="";
                 //send username and password off to loginEndpoint.php
                 RequestQueue reQueue = Volley.newRequestQueue(Register.this);
                 // Request a string response from the provided URL.
+
                 StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
                         new Response.Listener<String>() {
                             @Override
                             public void onResponse(String response) {
                                 Log.d("login response", response);
+                                try{
+                                    JSONObject json = new JSONObject(response);
+                                    if(json.has("username")){
+                                        Intent intent = new Intent(LoginService.LOGIN_BROADCAST);
+                                        intent.putExtra(LoginService.LOGIN_EXTRA, json.toString());
+                                        LocalBroadcastManager.getInstance(Register.this).sendBroadcast(intent);
+                                        //logged in, back to the mainActivity!
+                                        finish();
+                                    }
+                                }catch (JSONException e){
+                                    Log.e("jsonExceptio","Failed to register!", e);
+                                    Toast.makeText(Register.this, "Failed to register!",
+                                            Toast.LENGTH_LONG).show();
+                                }
                             }
+
 
                         }, new Response.ErrorListener() {
                     @Override
